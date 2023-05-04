4554d4ca821df43039a564ed9feeacf5496fb342
kkkkkkkkazuya <kkazuya.baseball0508@gmail.com>
2023-05-04 15:14:26 +0900

    add format

---
+++ envs/dev/variables.tf
@@ -0,0 +1,15 @@
+variable "name" {
+  default = "dev"
+}
+
+variable "region" {
+  default = "ap-northeast-1"
+}
+
+variable "vpc_cider" {
+  
+}
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "region"{}
---
+++ envs/prod/variables.tf
@@ -0,0 +1,11 @@
+variable "name" {
+  default = "prod"
+}
+
+variable "region" {
+  default = "ap-northeast-1"
+}
+
+variable "vpc_cider" {
+  
+}
