d84dbbbbb27de4a2ad2fd34dfa7ceb9df4879436
kkkkkkkkazuya <kkazuya.baseball0508@gmail.com>
2023-05-05 11:18:52 +0900

    add vpc igw,rt,associate

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -1,15 +1,33 @@
 variable "name" {
-  default = "dev"
+  type = string
 }
 
 variable "region" {
-  default = "ap-northeast-1"
+  type = string
 }
 
 variable "vpc_cider" {
-  
+  type = string
+}
+
+variable "az_a" {
+  type    = string
+  default = "ap-northeast-1a"
+}
+
+variable "az_c" {
+  type    = string
+  default = "ap-northeast-1c"
+}
+
+variable "public_dev_cidr_block" {
+  type = string
+}
+
+variable "dev_all_rt_cidr_block" {
+  type = string
 }
 
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
-variable "region"{}
+
--- envs/dev/vpc.tf
+++ envs/dev/vpc.tf
@@ -2,7 +2,7 @@
 # VPC
 # ---------------------------
 resource "aws_vpc" "dev_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cider
   # DNSホスト名有効化
   enable_dns_hostnames = true
   tags = {
@@ -14,9 +14,9 @@ resource "aws_vpc" "dev_vpc" {
 # サブネット
 # ---------------------------
 resource "aws_subnet" "public_dev_vpc_1a_sn" {
-  vpc_id = aws_vpc.dev_vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "${var.az_a}"
+  vpc_id            = aws_vpc.dev_vpc.id
+  cidr_block        = var.public_dev_cidr_block
+  availability_zone = var.az_a
 
   tags = {
     Name = "terraform_public_dev_vpc_1a_sn"
@@ -38,5 +38,18 @@ resource "aws_internet_gateway" "dev_igw" {
 # Route table
 # ---------------------------
 resource "aws_route_table" "dev_public_rt" {
-  vpc_id = aws
+  vpc_id = aws_vpc.dev_vpc.id
+  route {
+    cidr_block = dev_all_rt_cidr_block
+    gateway_id = aws_internet_gateway.dev_igw.id
+  }
+  tags = {
+    Name = "terraform-dev-public-rt"
+  }
+}
+
+# SubnetとRoute tableの関連付け
+resource "aws_route_table_association" "dev_public_rt_associate" {
+  subnet_id      = aws_subnet.public_dev_vpc_1a_sn.id
+  route_table_id = aws_route_table.dev_public_rt.id
 }
